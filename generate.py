464e169dd86eeff61587185f62d8283b9d184ce4
Tao Yang <swulling@gmail.com>
2023-06-08 13:32:09 +0800

    update

---
+++ generate.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+
+import os
+import sys
+import json
+
+issues = json.loads(sys.stdin.read())
+
+print("""# 九原山
+
+## Posts
+""")
+
+for issue in issues:
+    print(f"- #{issue['number']} [{issue['title']}]({issue['url']})] {issue['updatedAt'].split('T')[0]}")
+
